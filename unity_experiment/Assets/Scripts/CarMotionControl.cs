66a40262c3210657be34cd2e8e93cb3202d651a5
Dominic Eckstein <dominic.eckstein@hotmail.de>
2023-06-02 13:14:19 +0200

    Started commenting and reviewing scripts

--- unity_experiment/Assets/Scripts/CarMotionControl.cs
+++ unity_experiment/Assets/Scripts/CarMotionControl.cs
@@ -17,13 +17,8 @@ using UnityEngine;
 
 public class CarMotionControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-  
-        /*
-        public void speed/ acceleration: ariable für Geschwindigkeit/ Beschleunigung
-
-        */
       
+        //initialize the variables. The public variables can be then modified from the unity inspector of the game object that has the script attached.
         public float speed;
         [Tooltip("maximaler Radeinschlag in [grad]")]
         public float radeinschlag;
@@ -41,44 +36,54 @@ public class CarMotionControl : MonoBehaviour
     void Update()
     {
         
-        //Verbindung zum Lenkrad aufbauen
+        //Build a connection the logitech steering wheel (packages need to be installed!)
+        //For more infos see the README file in the repository
+
         LogitechGSDK.LogiControllerPropertiesData actualProperties = new LogitechGSDK.LogiControllerPropertiesData();
         LogitechGSDK.DIJOYSTATE2ENGINES rec;
         rec = LogitechGSDK.LogiGetStateUnity(0);
 
-       
+        //calculate the final rotation of the car in unity from the input of the steering wheel
+        //for the engineering calculations: lenkradumdrehungen, tatsächlicheÜbersetzung, radeinschlag see https://www.colliseum.eu/wiki/Lenkübersetzung 
+
+        //engineering calculations
         float lenkradumdrehungen = radeinschlag*übersetzung*2/360;
         float tatsächlicheÜbersetzung = übersetzung/lenkradumdrehungen;
-        float angleOfWheel = rec.lX;
-        float maximalX = 32768;
-        float angleOFWheelpercentage =angleOfWheel/maximalX;
-        //Debug.Log(angleOFWheelpercentage);
-        float maxDegreeOfSteer = 110;
-        float angle = angleOFWheelpercentage*maxDegreeOfSteer/tatsächlicheÜbersetzung;
-
-        //angle = (float)System.Math.Floor(angle);
-
-        angle = Mathf.Clamp(angle, -radeinschlag, radeinschlag);
 
 
-        //hier stimmt was nicht
+        float angleOfWheel = rec.lX; //get input from the steering wheel how many degrees it is turned
+        float maximalX = 32768;//maximal possible value of the steering wheel software
+        float angleOFWheelpercentage =angleOfWheel/maximalX;// get the proportionate rotation of the steering wheel
+        float maxDegreeOfSteer = 110;// maximalX is reached when the steering wheel is turned about 110 degree 
+
+
+        float angle = angleOFWheelpercentage*maxDegreeOfSteer/tatsächlicheÜbersetzung;// output angle that the car object will turn in unity
+
+        angle = Mathf.Clamp(angle, -radeinschlag, radeinschlag);//clamp the maximal possible angle by the radeinschlag
+
+        //This is the formulate to update the steering angle of the car object in each frame
+        //angle: actual angle of steering
+        //tempAngle: angle from last frame
+        //deltaAngle: difference between the actual angle and the angle from last frame
+        //newAngle: actual angle of the car
+
         deltaAngle = angle - tempAngle;
         newAngle = newAngle + Time.deltaTime * deltaAngle;
 
-        //newAngle = Mathf.Clamp(newAngle, -radeinschlag, radeinschlag);
-
-        float angleToRad = (float)(System.Math.PI / 180) * newAngle;
-        Vector3 steeringVec = new Vector3((float)System.Math.Sin(angleToRad), 0, (float)System.Math.Cos(angleToRad));
-        Vector3 steeringVecRotation = new Vector3(0, angleToRad, 0);
+        //In this section we calculate the actual movement of the car object in unity. Movement and rotation is based on Vector3 objects in unity
+     
+        float angleToRad = (float)(System.Math.PI / 180) * newAngle; //transform degree to radiant
+        Vector3 steeringVec = new Vector3((float)System.Math.Sin(angleToRad), 0, (float)System.Math.Cos(angleToRad));//Vector3 for the movement 
+        Vector3 steeringVecRotation = new Vector3(0, angleToRad, 0);//Vector3 for the rotation
 
     
+        //In this section the movement that we calculated from the user input is being combined with the actual car object
+        //The Time.timeScale function controls how fast time in the game runs where 1 is normal speed and 0 is a time stop. 
         
-        
-        
-        if(Time.timeScale == 1f & behindTheStartLine == true) //allow steering only while game is not paused
+        if(Time.timeScale == 1f & behindTheStartLine == true) //allow steering only while game is not paused and only if the car is behind the starting line
         {
             /*
-            This section might be of interest for another experiment, where the subject can control the speed of the vehicle
+            //This section might be of interest for another experiment, where the subject can control the speed of the vehicle via the pedals. For now we dont need it
 
 
             if (rec.lY <0)
@@ -92,6 +97,8 @@ public class CarMotionControl : MonoBehaviour
             transform.Translate(steeringVec*-speed* Time.deltaTime);    
             }
             */
+
+            //the transform function transforms the game object's position and rotation states
             transform.Translate(steeringVec*speed* Time.deltaTime); 
             transform.Rotate(steeringVecRotation);
         }
@@ -99,15 +106,19 @@ public class CarMotionControl : MonoBehaviour
         //only allow car to move but not to steer until car reaches starting line
         else if(Time.timeScale == 1f ){
 
+            //by setting all angles to 0 while the starting line has not been crossed yet the 
+            //participant is not able to steer the car. Only the transformation of the position is performed (car moving straigt)
             transform.Translate(Vector3.forward*speed* Time.deltaTime);
             tempAngle = 0;
             deltaAngle = 0;
             newAngle = 0;
         }
     
-        tempAngle = newAngle;
+           
+        tempAngle = newAngle; //save the actual angle for the calculations in the next frame
 
         //reset steering angle after every trial so that the car does not steer into the direction of the last trial
+        //In this state the game is paused via the timeScale indicating that the next trial has started.
         if(Time.timeScale == 0f){
             tempAngle = 0;
             deltaAngle = 0;
--- unity_experiment/Assets/Scripts/experimentalSetup.cs
+++ unity_experiment/Assets/Scripts/experimentalSetup.cs
@@ -11,12 +11,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-
+//This script performs the actual experiment procedure. To understand what is happening here I highly recommend the
+//tutorial on https://www.youtube.com/watch?v=1GGXz5XwPkk. To explain every step in this code would be way to complex so I will just give short overviews of the
+//different sections. Every step here is taken from the tutorial.
 
 
 public class experimentalSetup : MonoBehaviour
 {
-
+    //Set up all the game objects that are included into the experiment. These can then be assigned in the game inspector
     public GameObject car;
     public GameObject pole;
     Collider carCollider;
@@ -30,7 +32,7 @@ public class experimentalSetup : MonoBehaviour
     public int testcondition;
 
 
-
+    //initialie the trial with the starting positions of the car and the target
     public void  drivingTaskTrial(Trial trial)
     {
         carStartingPosition = car.transform.position;
@@ -38,17 +40,27 @@ public class experimentalSetup : MonoBehaviour
         StartCoroutine(drivingTaskSequence(trial));
     }
 
-    
+    //Here the real experiment magic happens. The Enumerator object can be understand as a procedure protocol of the experiment where each step is worked through step by step.
+    //It is initiated by a trial so that every trial follows exactly this procedure.
     IEnumerator drivingTaskSequence(Trial trial)
     {
         
+        //Here we initialize the trial with its settings. These settings are set in the UXF script and can be modified in the game inspector
         int condition = trial.settings.GetInt("condition");
         int targetspeed = trial.settings.GetInt("targetspeed");
+
+        //trial.result creates a row entry in the corresponding csv file that is created in the background when the experiments starts and saves the values
         trial.result["condition"] = condition;
         trial.result["targetspeed"] = targetspeed;
+
+        //The experimentator has no way of checking values while the experiment runs because the Occulus Rift crashes everytime a gameobject is being inspected.
+        //So the console output is a workaround for the important variables.
         Debug.Log("speed is"+ targetspeed);
         Debug.Log("condition is"+ condition);
         
+        //The condition variable is part of the UXF script. For more information go to the script. 
+        //The 0 condition represents the condition for the practice trials. In the practice block participants see either one of the experiment test conditions.
+        //with a probability of 50%.
         if(condition == 0)
         {
             System.Random chooseCase = new System.Random();
@@ -70,52 +82,62 @@ public class experimentalSetup : MonoBehaviour
 
 
         
-
+        //Rotation of the area in the beginning of each trial
         float randomAngle = (float)(new System.Random().NextDouble() * 180 + 90);
         environment.transform.rotation = Quaternion.Euler(0f, randomAngle, 0f);
 
         trial.result["map_rotation"] = randomAngle;
 
-
+        //Connect to the steering wheel again so participants can start the trial with pressing a button on it
         LogitechGSDK.LogiControllerPropertiesData actualProperties = new LogitechGSDK.LogiControllerPropertiesData();
         LogitechGSDK.DIJOYSTATE2ENGINES rec;
         rec = LogitechGSDK.LogiGetStateUnity(0);
 
-        //reset to starting point
+        //In this section we reset every game object to the starting positon again because they have been moved in the last trial
+
+        //coordinates of the target
         float x_target = -12f;
         float z_target = 70f;
-        float carTargetDistance = Vector2.Distance(new Vector2(0.0f, 0.0f),new Vector2(x_target, z_target));
-        
 
+
+        float carTargetDistance = Vector2.Distance(new Vector2(0.0f, 0.0f),new Vector2(x_target, z_target)); //this variable is used to check the distance between the car and the target
+        
+        //reset the car object to the origin of the map and the target to its starting position according to the coordinates above
         car.transform.position = Vector3.zero;
         car.transform.rotation = Quaternion.identity;
         car.GetComponent<Rigidbody>().velocity = Vector3.zero;
         car.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
         pole.transform.position = new Vector3(x_target,3f,z_target);
 
-
-        float minDist = 4f;
-        timeLeft = 30f;
-        //float targetspeed = pole.GetComponent<poleMoving>().speed;
+        //define the parameters that describe the conditions for ending a trial
+        float minDist = 4f;//distance between car and target when a hit is registered
+        timeLeft = 30f;//time of a trial
         
         Time.timeScale = 0f; // Pause the game when the canvas is shown
-        textObject.gameObject.SetActive(true); // Show the text canvas
+        textObject.gameObject.SetActive(true); // Show the text canvas that contains the instructions to start the next trial
 
+        //the WaitUntil function is a very good way of checking certain conditions. The game is paused while it is checked every frame 
+        //whether the conditions have been met or not. This is very valuable because Enumerator Objects dont have a update method
+        //here we wait until the button on the steering wheel has been pressed
         yield return new WaitUntil(() => 
         
            LogitechGSDK.LogiGetStateUnity(0).rgbButtons[4] == 128
             
         );
 
+        //after hitting the button the instruction text disappears
         textObject.gameObject.SetActive(false); // Hide the text canvas
         Time.timeScale = 1f; // Resume the game
 
+        //Now the part of the experiment starts in which objects start moving.
+
+
+        //The target does not move until the car crosses the starting line
         pole.GetComponent<poleMoving>().speed = 0;
 
         car.GetComponent<CarMotionControl>().behindTheStartLine = false;
 
-        //yield return new WaitForSeconds(3f);
-
+        //Wait until the car crosses the starting line. We set the minimal distance to 1m because the distance function used the center of objects
         yield return new WaitUntil(() => 
         
             Vector3.Distance(car.transform.position, startLine.transform.position) < 1
@@ -124,16 +146,17 @@ public class experimentalSetup : MonoBehaviour
 
         car.GetComponent<CarMotionControl>().behindTheStartLine = true;
 
+        //after the car crossed the finish line the target starts moving and the car can be steered
         pole.GetComponent<poleMoving>().speed = targetspeed;
 
-        //
+        //Now the trial is running and we wait for one of the condition that end the trial to be met.
         yield return new WaitUntil(() => 
-        
+            //Conditions to end a trial: target is hit / time is over / car missed the target
             Vector3.Distance(car.transform.position, pole.transform.position) <minDist | timeLeft <= 0 | car.transform.position.z > pole.transform.position.z+1
             
         );
 
-        //speichern ob target getroffen wurde oder nicht
+        //here we safe the value for the target being hit or not
         bool hit = false;
         if(Vector3.Distance(car.transform.position, pole.transform.position)<minDist)
         {
@@ -143,28 +166,21 @@ public class experimentalSetup : MonoBehaviour
         trial.result["hit"] = hit;
 
         trial.End();
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        
 
        
     }
     
     void Update(){
-
+        //this update method implements the timer. In every frame the duration of the frame is being subtracted from the time left
         timeLeft -= Time.deltaTime; 
     }
     
-   
+    //if the last trial has ended the time is being stopped and the session ends
     public void EndIfLastTrial(Trial trial)
     {
         if(trial == Session.instance.LastTrial)
         {
-            /*
-            car.transform.position = Vector3.zero;
-            car.transform.rotation = Quaternion.identity;
-            car.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            car.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            pole.transform.position = new Vector3(-46.2f,1.4f,55.4f);
-            */
             Time.timeScale = 0f;
             
 
