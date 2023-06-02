a8247ed4ebfc328009f5cbac5c689f99e0c4f500
Dominic Eckstein <dominic.eckstein@hotmail.de>
2023-06-02 14:53:44 +0200

    finished comments on the scripts

--- unity_experiment/Assets/Scripts/poleMoving.cs
+++ unity_experiment/Assets/Scripts/poleMoving.cs
@@ -2,56 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script controls the movement for the target
+    
 public class poleMoving : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
-    public float speed;
-    public uxfSetup uxfsetup;
-    public GameObject car;
-    public int timeToMove = 6;
-    public experimentalSetup experimentalSetup;
+    //initialize the values so that they can be modified in the inspector
 
-    // Update is called once per frame
+
+    public float speed;
+    public uxfSetup uxfsetup; //needed for the connection to other scripts
+    public GameObject car;
+    public int timeToMove = 6;//this variable is currently not used but can be another way of setting up the experiment
+    public experimentalSetup experimentalSetup;//needed for the connection to other scripts
+
 
     private void Update()
     {
-           //deltaTime converts from units per frame to units per second (because there are different frame rates on different devices plus different loading times)
-           //Vector.Direction are unit vectors 
-        
-        /*
-          if(uxfsetup.condition == 0){
-
-                System.Random chooseCase = new System.Random();
-
-                if (chooseCase.NextDouble() < 0.5)
-                {
-                 // Code line 1
-                    transform.Translate(Vector3.right*speed* Time.deltaTime); 
-                }
-
-                else
-                {
-                    // Code line 2
-                    double mean = 0;
-                    double stdDev = 7.0;
-                    // Create a new Random object
-                    System.Random random = new System.Random();
-
-                    // Generate a random value from a normal distribution
-                    double randomValue = mean + stdDev * System.Math.Sqrt(-2.0 * System.Math.Log(random.NextDouble())) * System.Math.Cos(2.0 * System.Math.PI * random.NextDouble());
-            
-                    // Add the random value to the value to add to
-                    speed = speed + Time.deltaTime*(float)randomValue;
-          
  
-                    transform.Translate(Vector3.right*(float)speed*Time.deltaTime);
-                }
-                
-
-          }
-
-          */
+          //in this section we check whether we are in condition 1(constant speed) or in condition 2(constant speed with addition of noise in every frame)
+          //and calculate the movement accordingly
 
           if(uxfsetup.condition == 1 | experimentalSetup.testcondition == 1)
           {
@@ -67,13 +36,13 @@ public class poleMoving : MonoBehaviour
             // Create a new Random object
             System.Random random = new System.Random();
 
-            // Generate a random value from a normal distribution
+            // Generate a random value from a normal distribution by using the Mueller Transformation
             double randomValue = mean + stdDev * System.Math.Sqrt(-2.0 * System.Math.Log(random.NextDouble())) * System.Math.Cos(2.0 * System.Math.PI * random.NextDouble());
             
             // Add the random value to the value to add to
             speed = speed + Time.deltaTime*(float)randomValue;
           
- 
+            //transform the position value of the target object. Time.deltaTime measures the duration of a frame 
             transform.Translate(Vector3.right*(float)speed*Time.deltaTime);
           }
 
--- unity_experiment/Assets/Scripts/uxfSetup.cs
+++ unity_experiment/Assets/Scripts/uxfSetup.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 using UXF;
 using System.Linq;
 
+//This scripts describes the "meta" structure/ logic of the UXF objects involved in the experiment. To understand what is happening here I highly recommend the
+//tutorial on https://www.youtube.com/watch?v=1GGXz5XwPkk. To explain every step in this code would be way to complex so I will just give short overviews of the
+//different sections. Every step here is taken from the tutorial.
 public class uxfSetup : MonoBehaviour
 {
-   // Start is called before the first frame update
-    public int numberOfTrialsInBlock;
+   
+    //initialize parameters of the experiment so that it can be modified in the inspector
+    public int numberOfTrialsInBlock; //number of trials of a block
     [Tooltip("1 für condition 1, 2 für condition 2 etc.")]
-    public int condition;
+
+    //We have three conditions: 0 = practice condition, 1 = condition with constant speed, 2 = condition with constant speed and noise
+    public int condition;// condition
     
 
     public void Generate(Session uxfSession)
@@ -18,9 +24,11 @@ public class uxfSetup : MonoBehaviour
 
         //List<int> speeds = new List<int>() { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6};
 
+        //This creates the list containing all trial conditions of a block. Above is the hard coded option
         List<int> speeds = Enumerable.Repeat(Enumerable.Range(4, 3).SelectMany(x => Enumerable.Repeat(x, numberOfTrialsInBlock/3)).ToArray(), 1).SelectMany(x => x).ToList();
         Debug.Log("speeds" + speeds);
 
+        //Here we pseudo randomize the trials by shuffling the list object
         System.Random rng = new System.Random();
         speeds = speeds.OrderBy(x => rng.Next()).ToList();
         Debug.Log(string.Join(",", speeds));
@@ -31,28 +39,16 @@ public class uxfSetup : MonoBehaviour
 
            
             
+            //here we pass the parameters for every trial. 
 
-            int speed = speeds[0];
+            //take first value of the shuffled list and then remove it
+            int speed = speeds[0]; 
             speeds.RemoveAt(0);
 
 
-            trial.settings.SetValue("targetspeed", speed);
-            trial.settings.SetValue("condition", condition);
+            trial.settings.SetValue("targetspeed", speed);//pass speed value
+            trial.settings.SetValue("condition", condition);//pass condition value
 
-
-
-            /*
-
-            if(condition == 1){
-                trial.settings.SetValue("Maximum Car Speed", 3);
-                trial.settings.SetValue("Maximum Pole Speed", 5);
-            }
-
-            if(condition == 2){
-                trial.settings.SetValue("Maximum Car Speed", 3);
-                trial.settings.SetValue("Maximum Pole Speed", 5);
-            }
-            */
         }
     }
 }
